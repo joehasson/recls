d1add0919910ce90d859737fe6563a8e97151941
joehasson <joe.hasson2@gmail.com>
2022-09-08 23:21:40 +0100

    Implement -a flag

--- recls.py
+++ recls.py
@@ -1,5 +1,6 @@
 """Print the """
 
+from ast import Store
 import os
 import argparse 
 from pathlib import Path
@@ -11,7 +12,9 @@ print = Console().print
 parser = argparse.ArgumentParser()
 parser.add_argument('path', type=str, nargs='?', default=os.getcwd(),
     help='Directory from which to search, default current')
-parser.add_argument('-d', '--depth', type=int, default=1, 
+parser.add_argument('-a', '--all', action='store_true',
+    help='Include files beginning with .')
+parser.add_argument('-d', '--depth', type=int, default=1,   #add mutual incompatible group for inf
     help='Depth to display nested files, default 2')
 parser.add_argument('-g', '--glob', type=str, default='*',
     help='Glob to match for')
@@ -28,23 +31,29 @@ def partition(pred, seq):
 partition_files_and_dirs =lambda i: partition(lambda p: p.is_dir(), i)
 
 
-def recursive_ls(path, max_depth, current_depth=0, indent = ""):
-    if current_depth >= max_depth:
+def recursive_ls(path, show_all, max_depth, current_depth=0):
+    if current_depth > max_depth:
         return
         
     dirs, files = partition_files_and_dirs(path.iterdir())
 
-    for d in filter(lambda v: not v.name.startswith('.'), dirs):
+    if not show_all:
+        dirs = filter(lambda d: not d.name.startswith('.'), dirs)
+        files = filter(lambda d: not d.name.startswith('.'), files)
+
+    indent = "  " * current_depth
+
+    for d in dirs:
         print(indent, '-', d.name, ':file_folder:', style='turquoise2')
-        recursive_ls(d, max_depth, current_depth+1, indent+"  ")
+        recursive_ls(d, show_all, max_depth, current_depth+1)
 
     for i, f in zip(range(5), files):
-        if not f.name.startswith('.'):
-            print(indent, '-', f.name)
-        if i == 9: print(f'{indent} .\n{indent} .\n{indent} .')
+        print(indent, '-', f.name)
+        if i == 4: 
+            print(f'{indent} .\n{indent} .\n{indent} .')
 
 
 
 if __name__ == '__main__':
     path = Path(args.path)
-    recursive_ls(path, args.depth)
+    recursive_ls(path, args.depth, args.all)
