d714807dd5862a6912bd096546eb9882493427eb
joehasson <joe.hasson2@gmail.com>
2022-10-20 23:17:57 +0100

    Some refactoring
    It seems altogether ill-advised to be mixing the type annotations for
    filter objects with the parameter name filters which contains a tuple of
    functions each of which takes an iterable and returns a filter function.
    Perhaps filters should be renamed something along the lines of
    "filterers".

--- recls.py
+++ recls.py
@@ -3,12 +3,12 @@
 import os
 import argparse 
 from pathlib import Path
+from typing import Tuple
 
 from rich import print # type: ignore
 from rich.tree import Tree # type: ignore
 
-from recls_utils import apply, is_dir, make_filter, partition_files_and_dirs
-
+import recls_utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path', type=str, nargs='?', default=os.getcwd(),
@@ -23,17 +23,21 @@ args = parser.parse_args()
 
 
 arg_to_filters = {
-    'all':  make_filter(lambda path: not path.name.startswith('.')),
-    'quiet': make_filter(is_dir)
+    'all':  recls_utils.filter_hidden_files_and_dirs,
+    'quiet': recls_utils.filter_out_files
 } 
 
 
-def build_tree(path, filters, t_depth, t= Tree('root', style='light_steel_blue')):
+def build_tree(path: Path,
+    filters: Tuple,
+    t_depth: int,
+    t: Tree=Tree('root', style='light_steel_blue')
+    ) -> None:
     """Generates the rich Tree object which represents directory structure"""
     try:
         if t_depth:
-            paths = apply(filters, path.iterdir())
-            dirs, files = partition_files_and_dirs(paths)
+            paths = recls_utils.apply(filters, path.iterdir())
+            dirs, files = recls_utils.partition_files_and_dirs(paths)
             add_dir_branches(dirs, filters, t_depth-1, t)
             add_file_branches(files, t)
         return t
@@ -41,18 +45,19 @@ def build_tree(path, filters, t_depth, t= Tree('root', style='light_steel_blue')
         print(e)
 
 
-def add_dir_branches(dirs, filters, branch_depth, t) -> None:
+def add_dir_branches(dirs: filter, filters: Tuple, branch_depth, t) -> None:
     """Add branches to tree for each directory in path in-place"""
     for d in sorted(dirs):
         branch = t.add(f'[bold bright_cyan]{d.name}[/]' + " 📁")
         build_tree(d, filters, branch_depth, branch)
 
 
-def add_file_branches(files, t):
+def add_file_branches(files, t) -> None:
+    """Add branches to tree for each file in path in-place up to max of five"""
     for i, f in (iterator:= enumerate(sorted(files))):
         t.add(f.name, style='white')
-        if i == 4 and (num_left := (sum(1 for _ in iterator))):
-             t.add(f'+{num_left} others', style='yellow')
+        if i == 4 and (num_files_left := (sum(1 for _ in iterator))):
+             t.add(f'+{num_files_left} others', style='yellow')
 
 
 if __name__ == '__main__':
