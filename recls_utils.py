40e739523b10662fd55ee24c5d36bf087fd03b49
joehasson <joe.hasson2@gmail.com>
2022-10-21 00:21:41 +0100

    Fix whitespace

--- recls_utils.py
+++ recls_utils.py
@@ -6,14 +6,14 @@ from typing import Any, Callable, Iterable, Tuple
 def apply(funcs: Iterable[Callable], arg: Any) -> Any:
     """Call functions in funcs from left to right on arg and return result"""
     return reduce(lambda acc, f: f(acc), funcs, arg)
-        
+
 def make_filter(pred: Callable[[Any], bool]) -> Callable[[Iterable], filter]:
     """Take predicate function and return a function which applies corresponding filter"""
     return lambda x: filter(pred, x)
 
 def partition(pred: Callable[[Any], bool], seq: Iterable) -> Tuple[filter, filter]:
     """Take a function pred and a sequence seq, return two filter objects containing
-    elements of seq, the first (resp. second) containing those elements of seq 
+    elements of seq, the first (resp. second) containing those elements of seq
     for which pred returns True (False)"""
     seq_copy1, seq_copy2 = tee(seq)
     return filter(pred, seq_copy1), filter(lambda v: not pred(v), seq_copy2)
